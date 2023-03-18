15fbd8ad17fccb02030c7472397f87041d294cc1
Wilka <wcarvalho92@gmail.com>
2023-03-17 20:03:36 -0400

    more control over r2d2 epsilon schedule

--- r2d2.py
+++ r2d2.py
@@ -2,13 +2,20 @@ import functools
 
 from typing import Optional, Tuple, Iterator, Optional
 
+import acme
+from acme import adders
+
 from acme import core
-from acme.jax.networks import base
 from acme import specs
+from acme.agents.jax import actors
+from acme.agents.jax import actor_core as actor_core_lib
 from acme.agents.jax import r2d2
+from acme.agents.jax.r2d2 import actor as r2d2_actor
 from acme.agents.jax.r2d2 import learning as r2d2_learning
 from acme.agents.jax.r2d2 import networks as r2d2_networks
+from acme.jax.networks import base
 from acme.jax import networks as networks_lib
+from acme.jax import variable_utils
 from acme.jax.networks import duelling
 from acme.utils import counting
 from acme.utils import loggers
@@ -16,8 +23,11 @@ from acme.wrappers import observation_action_reward
 
 import dataclasses
 import haiku as hk
+import jax
+import numpy as np
 import optax
 import reverb
+import rlax
 
 from modules import vision
 from modules import language
@@ -34,6 +44,12 @@ class R2D2Config(r2d2.R2D2Config):
   state_dim: int = 256
   q_dim: int = 512
   conv_out_dim: int = 0
+
+  # value-based action-selection options
+  num_epsilons: int = 256
+  epsilon_min: float = 1e-2
+  epsilon_max: float = 1
+  epsilon_base: float = .1
 
   # Learner options
   discount: float = 0.99
@@ -141,6 +157,89 @@ class R2D2Builder(r2d2.R2D2Builder):
         counter=counter,
         logger=logger_fn('learner'))
 
+  def make_actor(
+      self,
+      random_key: networks_lib.PRNGKey,
+      policy: r2d2_actor.R2D2Policy,
+      environment_spec: specs.EnvironmentSpec,
+      variable_source: Optional[core.VariableSource] = None,
+      adder: Optional[adders.Adder] = None,
+  ) -> acme.Actor:
+    del environment_spec
+    # Create variable client.
+    variable_client = variable_utils.VariableClient(
+        variable_source,
+        key='actor_variables',
+        update_period=self._config.variable_update_period)
+
+    return actors.GenericActor(
+        policy, random_key, variable_client, adder, backend='cpu')
+
+  def make_policy(self,
+                  networks: r2d2_networks.R2D2Networks,
+                  environment_spec: specs.EnvironmentSpec,
+                  evaluation: bool = False) -> r2d2_actor.R2D2Policy:
+
+    return get_actor_core(
+        networks,
+        config=self._config,
+        evaluation=evaluation)
+
+
+def get_actor_core(
+    networks: r2d2_networks.R2D2Networks,
+    config: R2D2Config,
+    evaluation: bool = False,
+) -> r2d2_actor.R2D2Policy:
+  """Returns ActorCore for R2D2."""
+
+  num_epsilons = config.num_epsilons
+  evaluation_epsilon = config.evaluation_epsilon
+  if (not num_epsilons and evaluation_epsilon is None) or (num_epsilons and
+                                                           evaluation_epsilon):
+    raise ValueError(
+        'Exactly one of `num_epsilons` or `evaluation_epsilon` must be '
+        f'specified. Received num_epsilon={num_epsilons} and '
+        f'evaluation_epsilon={evaluation_epsilon}.')
+
+  def select_action(params: networks_lib.Params,
+                    observation: networks_lib.Observation,
+                    state: r2d2.R2D2ActorState[actor_core_lib.RecurrentState]):
+    rng, policy_rng = jax.random.split(state.rng)
+
+    q_values, recurrent_state = networks.apply(params, policy_rng, observation,
+                                               state.recurrent_state)
+    action = rlax.epsilon_greedy(state.epsilon).sample(policy_rng, q_values)
+
+    return action, r2d2.R2D2ActorState(
+        rng=rng,
+        epsilon=state.epsilon,
+        recurrent_state=recurrent_state,
+        prev_recurrent_state=state.recurrent_state)
+
+  def init(
+      rng: networks_lib.PRNGKey
+  ) -> r2d2.R2D2ActorState[actor_core_lib.RecurrentState]:
+    rng, epsilon_rng, state_rng = jax.random.split(rng, 3)
+    if not evaluation:
+      epsilon = jax.random.choice(epsilon_rng,
+                                  np.logspace(config.epsilon_min, config.epsilon_max, config.num_epsilons, base=config.epsilon_base))
+    else:
+      epsilon = evaluation_epsilon
+    initial_core_state = networks.init_recurrent_state(state_rng, None)
+    return r2d2.R2D2ActorState(
+        rng=rng,
+        epsilon=epsilon,
+        recurrent_state=initial_core_state,
+        prev_recurrent_state=initial_core_state)
+
+  def get_extras(
+      state: r2d2.R2D2ActorState[actor_core_lib.RecurrentState]) -> r2d2.R2D2Extras:
+    return {'core_state': state.prev_recurrent_state}
+
+  return actor_core_lib.ActorCore(init=init, select_action=select_action,
+                                  get_extras=get_extras)
+
 
 def make_r2d2_babyai_networks(
         env_spec: specs.EnvironmentSpec,
@@ -149,8 +248,7 @@ def make_r2d2_babyai_networks(
 
   num_actions = env_spec.actions.num_values
   def make_core_module() -> R2D2Arch:
-    vision_torso = vision.BabyAIVisionTorso(conv_dim=config.conv_out_dim,
-                                            flatten=False)
+    vision_torso = vision.BabyAIVisionTorso(conv_dim=config.conv_out_dim)
     task_encoder = language.LanguageEncoder(
             vocab_size=config.vocab_size,
             word_dim=config.word_dim,
